9ea121e7c762184f270128f7a6f74f933a82a850
AarshShah9 <aarshshah99@gmail.com>
2024-02-13 10:09:35 -0700

    Added login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './views/Main';
+import LoginPage from "./views/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
+          <Route path="/login" element={<LoginPage   />} />
       </Routes>
     </div>
   );
