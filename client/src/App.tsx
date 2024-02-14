addcbd12ad7e11cf6da3515f756a356efe488917
HylianMuffins <catholiczach@gmail.com>
2024-02-13 22:52:52 -0700

    made a landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Main from './views/Main';
-import LoginPage from "./views/LoginPage";
+import LoginPage from './views/LoginPage';
+import LandingPage from './views/LandingPage';
+import NotFound from './views/NotFount';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
-          <Route path="/login" element={<LoginPage   />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/landing" element={<LandingPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
