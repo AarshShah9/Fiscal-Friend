e253190dd3cea1230a94179925ee6421689c212a
fraserwill03 <williamgfraser2015@gmail.com>
2024-03-05 20:01:05 -0700

    landing, login, signup, and notfound styling

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { LoginData, useAuth } from '../contexts/AuthContext';
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
   const { register, handleSubmit } = useForm<LoginData>();
@@ -18,11 +18,16 @@ export default function Login() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <Link to="/">
+            <img
+              className="mx-auto h-20 w-auto"
+              src="/images/logo.png"
+              alt="Fiscal-Friend logo"
+            />
+          </Link>
+          <h1 className="mt-10 text-center text-5xl font-bold leading-9 tracking-tight text-emerald-500">
+            Welcome back friend!
+          </h1>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
@@ -44,7 +49,7 @@ export default function Login() {
                   autoComplete="email"
                   required
                   {...register('email')}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -60,7 +65,7 @@ export default function Login() {
                 <div className="text-sm">
                   <a
                     href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    className="font-semibold text-emerald-500 hover:text-emerald-400"
                   >
                     Forgot password?
                   </a>
@@ -73,7 +78,7 @@ export default function Login() {
                   autoComplete="current-password"
                   required
                   {...register('password')}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -81,7 +86,7 @@ export default function Login() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
               >
                 Sign in
               </button>
@@ -91,8 +96,8 @@ export default function Login() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{' '}
             <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              href="/signup"
+              className="font-semibold leading-6 text-emerald-500 hover:text-emerald-400"
             >
               Sign up now
             </a>
--- client/src/views/Signup.tsx
+++ client/src/views/Signup.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { SignupData, useAuth } from '../contexts/AuthContext';
 
 export default function Signup() {
-  const { register, handleSubmit, watch, setError, clearErrors, formState: { errors } } = useForm<SignupData>();
+  const { register, handleSubmit, setError, clearErrors, formState: { errors } } = useForm<SignupData>();
   const { signup, isLoading, isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
@@ -30,11 +30,16 @@ export default function Signup() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <Link to="/">
+            <img
+              className="mx-auto h-20 w-auto"
+              src="/images/logo.png"
+              alt="Fiscal-Friend logo"
+            />
+          </Link>
+          <h1 className="mt-10 text-center text-5xl font-bold leading-9 tracking-tight text-emerald-500">
+            Join the Friend Group
+          </h1>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign up for an account
           </h2>
@@ -53,7 +58,7 @@ export default function Signup() {
                   autoComplete="given-name"
                   required
                   {...register('firstName')}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -69,7 +74,7 @@ export default function Signup() {
                   autoComplete="family-name"
                   required
                   {...register('lastName')}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -85,7 +90,7 @@ export default function Signup() {
                   autoComplete="email"
                   required
                   {...register('email')}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -101,7 +106,7 @@ export default function Signup() {
                   autoComplete="new-password"
                   required
                   {...register('password')}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -117,7 +122,7 @@ export default function Signup() {
                   autoComplete="new-password"
                   required
                   {...register('confirmPassword')}
-                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${errors.confirmPassword ? 'border-red-500' : ''}`}
+                  className={`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-500 sm:text-sm sm:leading-6 ${errors.confirmPassword ? 'border-red-500' : ''}`}
                 />
                 {errors.confirmPassword && (
                   <p className="text-sm text-red-500 mt-1">{errors.confirmPassword.message}</p>
@@ -128,7 +133,7 @@ export default function Signup() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-emerald-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-500"
               >
                 Sign up
               </button>
@@ -137,7 +142,7 @@ export default function Signup() {
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Already have an account?{' '}
-            <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            <a href="/login" className="font-semibold leading-6 text-emerald-600 hover:text-emerald-500">
               Sign in
             </a>
           </p>
