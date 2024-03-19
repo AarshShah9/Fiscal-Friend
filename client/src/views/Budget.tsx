f0d125dbc900dc300c99d6f741786d8140c36589
fraserwill03 <williamgfraser2015@gmail.com>
2024-03-19 15:47:41 -0600

    buttons for sidebar

--- client/src/views/Budget.tsx
+++ client/src/views/Budget.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PieChart from '../components/PieChart';
 import StatsBoxes from '../components/StatsBoxes';
 import TransactionsTable from '../components/TransactionsTable';
+import Sidebar from '../components/Sidebar';
 
 type ExpenseItem = { [key: string]: number };
 
-type Budget = {
+type IBudget = {
   income: number;
   expenses: {
     total: number;
@@ -13,7 +14,7 @@ type Budget = {
   };
 };
 
-const budget: Budget = {
+const budget: IBudget = {
   income: 3000,
   expenses: {
     total: 1600,
@@ -21,15 +22,35 @@ const budget: Budget = {
   },
 };
 
-export const BudgetContext = React.createContext<Budget>(budget);
+export const BudgetContext = React.createContext<IBudget>(budget);
 
 export default function Budget() {
-    return (
-        <BudgetContext.Provider value={budget}>
-            <PieChart />
-            <StatsBoxes />
-            <div className="min-h-10"></div>
-            <TransactionsTable />
-        </BudgetContext.Provider>
-    );
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+  }
+
+  return (
+    <BudgetContext.Provider value={budget}>
+      <div className="flex flex-row flex-wrap-reverse">
+        {/* First Column: Pie Chart */}
+        <div className="grow">
+          <PieChart />
+        </div>
+        <div className="flex flex-col justify-start justify-items-center">
+          <button className="bg-emerald-500 hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded-full mb-4" onClick={toggleSidebar}>
+            Button 1
+          </button>
+          <button className="bg-emerald-500 hover:bg-emerald-400 text-white font-bold py-2 px-4 rounded-full" onClick={toggleSidebar}>
+            Button 2
+          </button>
+        </div>
+        <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar}/>
+      </div>
+      <StatsBoxes />
+      <div className="min-h-10"></div>
+      <TransactionsTable />
+    </BudgetContext.Provider>
+  );
 }
