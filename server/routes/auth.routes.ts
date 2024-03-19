9e515ef85fd7c2776d1d71bbdb52888cf0a0f54a
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2024-03-19 16:47:40 -0600

    didn't really do anything honestly

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express';
 
-import { checkAuth } from "../middleware/checkAuth";
-import { login, register, logout, me } from "../controllers/auth.controller";
+import { checkAuth } from '../middleware/checkAuth';
+import { login, register, logout, me } from '../controllers/auth.controller';
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", checkAuth, logout);
-router.post("/me", checkAuth, me);
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', checkAuth, logout);
+router.post('/me', checkAuth, me);
 
 export default router;
