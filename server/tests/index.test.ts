6a476fd1c7a455bee711b394ba6b9005d4e1428e
fraserwill03 <williamgfraser2015@gmail.com>
2024-03-19 10:09:27 -0600

    expense and income tests finished

--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -9,6 +9,7 @@ const agent = request.agent(app);
 // Import tests
 import { authTests } from './auth';
 import { incomeTests } from './income';
+import { expenseTests } from './expense';
 
 let mongoClient: Connection;
 
@@ -37,6 +38,7 @@ beforeAll(async () => {
 describe('Fiscal Friend API Tests', () => {
   authTests();
   incomeTests(agent);
+  expenseTests(agent);
 });
 
 afterAll(async () => {
