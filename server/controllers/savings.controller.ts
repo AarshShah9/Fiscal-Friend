51f34b273acdac73f501f0c58f1e28f3db7b8892
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2024-03-19 16:46:03 -0600

    created controller for savings

---
+++ server/controllers/savings.controller.ts
@@ -0,0 +1,60 @@
+import User from '../models/User';
+import { SavingsModel } from '../models/SavingsModel';
+import { Request, Response } from 'express';
+
+export const createSavings = async (req: Request, res: Response) => {
+  if (!req.body.user) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User not authenticated' });
+  }
+
+  const user = await User.findById(req.body.user);
+
+  if (!user) {
+    return res.status(400).json({ success: false, message: 'User not found' });
+  }
+
+  if (!req.body.name || !req.body.chequing || !req.body.savings) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Invalid request body' });
+  }
+
+  const newSavings = new SavingsModel({
+    user: req.body.user,
+    name: req.body.name,
+    savingAccount: {
+      chequing: req.body.chequing,
+      savings: req.body.savings,
+      resp: req.body.resp,
+    },
+    loanAccount: {
+      loc: req.body.loc,
+      mortgage: req.body.mortgage,
+    },
+  });
+
+  user.Savings.push(newSavings._id);
+
+  await user.save();
+
+  try {
+    await newSavings.save();
+    return res
+      .status(201)
+      .json({ success: true, message: 'Savings created', expense: newSavings });
+  } catch (e) {
+    return res.status(500).json({ success: false, message: 'Server error', e });
+  }
+};
+
+export const getSavings = async (req: Request, res: Response) => {
+  if (!req.body.user) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User not authenticated' });
+  }
+  const savings = await SavingsModel.find({ user: req.body.user });
+  return res.status(201).json({ success: true, savings });
+};
