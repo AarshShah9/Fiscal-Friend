f97664182701c3d5422ba0beafc29b149f50430a
EadanL <eadan.lay@ucalgary.ca>
2024-03-20 16:31:16 -0600

    added routes for new controller methods + tweaks

--- server/routes/stock.routes.ts
+++ server/routes/stock.routes.ts
@@ -1,9 +1,17 @@
 import express from 'express';
-import { getStockData } from '../controllers/stock.controller';
+import {
+  getSavedStocks,
+  getStockData,
+  removeStock,
+  saveFavouriteSymbol,
+} from '../controllers/stock.controller';
 import { checkAuth } from '../middleware/checkAuth';
 
 const router = express.Router();
 
-router.post('/test', checkAuth, getStockData);
+router.post('/search', checkAuth, getStockData);
+router.post('/save', checkAuth, saveFavouriteSymbol);
+router.post('/get', checkAuth, getSavedStocks);
+router.post('/remove', checkAuth, removeStock);
 
 export default router;
