f90421ee1a2472c9cb29418d3dabd670808ddcf1
EadanL <eadan.lay@ucalgary.ca>
2024-03-19 19:20:41 -0600

    changing reponse type

--- server/controllers/stock.controller.ts
+++ server/controllers/stock.controller.ts
@@ -1,6 +1,15 @@
 require('dotenv').config({ path: '../../.env' });
 import { Request, Response } from 'express';
 
+interface StockData {
+  Date: Date;
+  Open: number;
+  High: number;
+  Low: number;
+  Close: number;
+  Volume: number;
+}
+
 export const getStockData = (req: Request, res: Response) => {
   if (!req.body.symbol) {
     return res.status(400).json({ success: false, message: 'Missing symbol' });
@@ -9,12 +18,15 @@ export const getStockData = (req: Request, res: Response) => {
 
   fetch(url)
     .then((res) => res.json())
-    .then((data: any) => {
+    .then((data: JSON) => {
+      // const dataList: StockData[] = formatStockDataList(data);
       res.status(200).json(data);
     })
     .catch((err) => {
       console.log(err);
     });
 };
+
+// const formatStockDataList = (data: JSON): StockData[] => {};
 
 // export default {getStockData}
