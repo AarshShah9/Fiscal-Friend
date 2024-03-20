455ef1f7a000a5c17ed3e4b510fce228024ce7c6
Ken <ken.liu1@ucalgary.ca>
2024-03-19 21:43:15 -0600

    Added routing to Savings

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -297,7 +297,7 @@ export default function Layout() {
             </div>
           </div>
 
-          <main className="xl:pl-96">
+          <main className="xl:pl-12">
             <div className="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
               <Outlet />
               {/* Main area */}
--- client/src/views/Savings.tsx
+++ client/src/views/Savings.tsx
@@ -8,7 +8,7 @@ const Savings = () => {
     "loans": 0,
     "debts": 0
   });
-  console.log(userSavings)
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleSavingsChange = (e: { target: { value: any; }; }) => {
     setUserSavings({
@@ -34,45 +34,8 @@ const Savings = () => {
   return (
     <div>
       <div>
-        <h1 className="text-5xl pb-2">Savings</h1>
-        <p>To get started on tracking your savings on Fiscal Friend, please enter the information below!</p>
-        <p className="pb-12">(The information may be changed later.)</p>
+        <p>Stinky poo</p>
       </div>
-      <form>
-        <div className="pb-8">
-          <label htmlFor="savings" className="block text-sm font-medium leading-6 text-gray-900">Savings</label>
-          <div className="relative mt-2 rounded-md shadow-sm">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <span className="text-gray-500 sm:text-sm">$</span>
-            </div>
-            <input type="text" name="savings" onChange={handleSavingsChange} className="block w-50 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"></input>
-          </div>
-        </div>
-        <div className="pb-8">
-          <label htmlFor="debt" className="block text-sm font-medium leading-6 text-gray-900">Loans</label>
-          <div className="relative mt-2 rounded-md shadow-sm">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <span className="text-gray-500 sm:text-sm">$</span>
-            </div>
-            <input type="text" name="debt" onChange={handleDebtChange} className="block w-50 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"></input>
-          </div>
-        </div>
-        <div className="pb-8">
-          <label htmlFor="loan" className="block text-sm font-medium leading-6 text-gray-900">Debts</label>
-          <div className="relative mt-2 rounded-md shadow-sm py-">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <span className="text-gray-500 sm:text-sm">$</span>
-            </div>
-            <input type="text" name="loan" onChange={handleLoanChange} className="block w-50 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"></input>
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="flex w-50 justify-center rounded-md bg-emerald-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Continue
-        </button>
-      </form>
     </div>
   );
 };
