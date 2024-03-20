f9170cd6e954e093347eb9faecc43b1742afdc18
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2024-03-20 15:11:30 -0600

    filled in mortgage schema

--- server/models/Mortgage.ts
+++ server/models/Mortgage.ts
@@ -1,15 +1,49 @@
-import mongoose from 'mongoose';
+import mongoose, { model, Schema, Model, Document } from 'mongoose';
+
+interface IMortgage extends Document {
+  user: mongoose.Types.ObjectId;
+  mortgage: {
+    amount: Number;
+    apr: Number;
+    period: Number;
+  };
+  payments: {
+    principal: Number;
+    ear: Number;
+    repayment: Number;
+    total: Number;
+  };
+  frequency:
+    | 'Bi-Weekly (every 2 weeks)'
+    | 'Semi-Monthly (24x per year)'
+    | 'Monthly (12x per year)';
+}
 
 const mortgageSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  propertyAddress: { type: String, required: true },
-  loanAmount: { type: Number, required: true },
-  interestRate: Number,
-  monthlyPayment: Number,
-  startDate: Date,
-  endDate: Date,
+  mortgage: {
+    amount: { type: Number, required: true },
+    apr: { type: Number, required: true },
+    period: { type: Number, required: true },
+  },
+  payments: {
+    principal: { type: Number, required: false },
+    ear: { type: Number, required: false },
+    repayment: { type: Number, required: false },
+    total: { type: Number, required: false },
+  },
+  frequency: {
+    type: String,
+    required: true,
+    enum: [
+      'Bi-Weekly (every 2 weeks)',
+      'Semi-Monthly (24x per year)',
+      'Monthly (12x per year)',
+    ],
+  },
 });
 
-const Mortgage = mongoose.model('Mortgage', mortgageSchema);
-
-export default Mortgage;
+export const Mortgage: Model<IMortgage> = model<IMortgage>(
+  'Mortgage',
+  mortgageSchema
+);
