6ec080a0bb347477804a04b48132355ac88d24c6
EadanL <eadan.lay@ucalgary.ca>
2024-03-21 13:38:17 -0600

    added route for new controller method

--- server/routes/stock.routes.ts
+++ server/routes/stock.routes.ts
@@ -3,6 +3,7 @@ import {
   getSavedStocks,
   getStockData,
   removeStock,
+  requestUserFavorites,
   saveSymbol,
 } from '../controllers/stock.controller';
 import { checkAuth } from '../middleware/checkAuth';
@@ -13,5 +14,6 @@ router.post('/search', checkAuth, getStockData);
 router.post('/save', checkAuth, saveSymbol);
 router.post('/get', checkAuth, getSavedStocks);
 router.post('/remove', checkAuth, removeStock);
+router.post('/searchFavourites', checkAuth, requestUserFavorites);
 
 export default router;
