41c37a9ce5c78ffa5f78808ddb18b06800946a1f
fraserwill03 <williamgfraser2015@gmail.com>
2024-03-21 12:07:40 -0600

    mistake fix

--- server/controllers/income.controller.ts
+++ server/controllers/income.controller.ts
@@ -10,7 +10,7 @@ export const createIncome = async (req: Request, res: Response) => {
         return res.status(400).json({ success: false, message: 'Invalid request body' });
     }
 
-    if (req.body.recurring !== 'One-time' && !req.body.date) {
+    if (req.body.recurring === 'One-time' && !req.body.date) {
         return res.status(400).json({ success: false, message: 'Invalid request body' });
     }
 
