b7ff5e92b5d227d930b65f03687209ddc55c457f
AarshShah9 <aarshshah99@gmail.com>
2024-03-22 14:19:52 -0600

    Cleaned up fe

---
+++ client/src/views/Team.tsx
@@ -0,0 +1,49 @@
+const people = [
+  {
+    name: 'Aarsh Shah',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg',
+  },
+];
+
+export default function Team() {
+  return (
+    <div className="bg-white py-24 sm:py-32 min-h-screen">
+      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-2xl pr-6">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Meet our leadership
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            The team at Fiscal Friend is dedicated to helping you build wealth.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-x-6">
+                <img
+                  className="h-16 w-16 rounded-full"
+                  src={person.imageUrl}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                    {person.name}
+                  </h3>
+                  <p className="text-sm font-semibold leading-6 text-indigo-600">
+                    {person.role}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
