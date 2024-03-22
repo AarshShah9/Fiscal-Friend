308613cd50164818f7ff8e141dda2191719a1ea2
AarshShah9 <aarshshah99@gmail.com>
2024-03-22 10:41:45 -0600

    attempting 2

--- server/app.ts
+++ server/app.ts
@@ -27,6 +27,7 @@ app.use(
       'http://localhost:3000',
       'http://localhost',
       'https://fiscal-friend.vercel.app',
+      'https://fiscal-friend-git-aarsh-fix-aws-aarshshah9.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
