4e44793cc86be886b97b55e94b76036493fb67cd
AarshShah9 <aarshshah99@gmail.com>
2024-03-22 10:40:05 -0600

    attempting

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,6 +1,6 @@
 const URL =
   process.env.REACT_APP_ENV === 'production'
-    ? 'https://fiscal-friend.ca-central-1.elasticbeanstalk.com'
+    ? 'https://ffff.ca-central-1.elasticbeanstalk.com'
     : 'http://localhost:4000';
 
 export { URL };
