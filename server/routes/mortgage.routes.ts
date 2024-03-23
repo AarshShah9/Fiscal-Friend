4b5000f60f8921dd13921cf1e4e8a81e298c8346
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2024-03-22 22:43:29 -0600

    added PUT route

--- server/routes/mortgage.routes.ts
+++ server/routes/mortgage.routes.ts
@@ -3,11 +3,13 @@ import { checkAuth } from '../middleware/checkAuth';
 import {
   createMortgage,
   getMortgage,
+  updateMortgage,
 } from '../controllers/mortgage.controller';
 
 const router = express.Router();
 
 router.post('/create', checkAuth, createMortgage);
 router.get('/get', checkAuth, getMortgage);
+router.put('/update', checkAuth, updateMortgage);
 
 export default router;
