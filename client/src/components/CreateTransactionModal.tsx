fd4063c641662679eb66a46a145f31169f677c93
AarshShah9 <aarshshah99@gmail.com>
2024-03-23 23:23:42 -0600

    Rmved modal changes:

--- client/src/components/CreateTransactionModal.tsx
+++ client/src/components/CreateTransactionModal.tsx
@@ -1,13 +1,14 @@
-import React, { Fragment, useRef, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import React, { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { URL } from '../utils/constants';
 
+
 interface CreateTransactionModalProps {
   isOpen: boolean;
   onClose: () => void;
-  type: 'expenses' | 'incomes' | undefined;
+  type: "expenses" | "incomes" | undefined;
   incomes: any[];
   expenses: any[];
   setRefreshRequired: React.Dispatch<React.SetStateAction<boolean>>;
@@ -28,17 +29,10 @@ type expenseForm = {
   category: string;
 };
 
-const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({
-  isOpen,
-  onClose,
-  type,
-  incomes,
-  expenses,
-  setRefreshRequired,
-}) => {
+const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({ isOpen, onClose, type, incomes, expenses, setRefreshRequired }) => {
   const { register, handleSubmit, watch } = useForm<expenseForm | incomeForm>({
     defaultValues: {
-      name: 'New',
+      name: "New",
       amount: 0,
       date: new Date(),
       recurring: 'One-time',
@@ -51,25 +45,25 @@ const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({
   const handleClose = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation(); // Prevent propagation of click event to parent elements
     onClose(); // Close the modal
-  };
+    };
 
-  const onSubmit = (data: incomeForm | expenseForm) => {
-    if (type === 'expenses') {
-      const expenseFormData: expenseForm = data as expenseForm;
-      axios.post(`${URL}/expense/create`, expenseFormData).then((res) => {
-        expenses.push(res.data.expense);
-        setRefreshRequired(true);
-        onClose();
-      });
-    } else {
-      const incomeFormData: incomeForm = data as incomeForm;
-      axios.post(`${URL}/income/create`, incomeFormData).then((res) => {
-        incomes.push(res.data.income);
-        setRefreshRequired(true);
-        onClose();
-      });
-    }
-  };
+    const onSubmit = (data: incomeForm | expenseForm) => {
+      if (type === 'expenses') {
+        const expenseFormData: expenseForm = data as expenseForm;
+        axios.post(`${URL}/expense/create`, expenseFormData).then((res) => {
+          expenses.push(res.data.expense);
+          setRefreshRequired(true);
+          onClose();
+        });
+      } else {
+        const incomeFormData: incomeForm = data as incomeForm;
+        axios.post(`${URL}/income/create`, incomeFormData).then((res) => {
+          incomes.push(res.data.income);
+          setRefreshRequired(true);
+          onClose();
+        });
+      }
+    };
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-50" onClose={onClose}>
@@ -100,16 +94,11 @@ const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        {type === 'expenses'
-                          ? 'Create New Expense'
-                          : 'Create New Income'}
+                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        {type === 'expenses' ? 'Create New Expense' : 'Create New Income'}
                       </Dialog.Title>
                       <div className="mt-2">
-                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                           <div className="sm:col-span-3">
                             <label
                               htmlFor="name"
@@ -151,7 +140,7 @@ const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({
                               Recurring?
                             </label>
                             <div className="mt-2">
-                              <select
+                            <select
                                 id="recurring"
                                 {...register('recurring')}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
@@ -184,41 +173,37 @@ const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({
                               </div>
                             </div>
                           )}
-                          {type === 'expenses' && (
-                            <div className="sm:col-span-3">
-                              <label
-                                htmlFor="Category"
-                                className="block text-sm font-medium leading-6 text-gray-900"
-                              >
-                                Category
-                              </label>
-                              <div className="mt-2">
-                                <select
-                                  id="category"
-                                  {...register('category')}
-                                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
+                            {type === 'expenses' && (
+                              <div className="sm:col-span-3">
+                                <label
+                                  htmlFor="Category"
+                                  className="block text-sm font-medium leading-6 text-gray-900"
                                 >
-                                  <option value="Food">Food</option>
-                                  <option value="Utilities">Utilities</option>
-                                  <option value="Rent">Rent</option>
-                                  <option value="Transportation">
-                                    Transportation
-                                  </option>
-                                  <option value="Insurance">Insurance</option>
-                                  <option value="Wellness">Wellness</option>
-                                  <option value="Entertainment">
-                                    Entertainment
-                                  </option>
-                                  <option value="Other">Other</option>
-                                </select>
+                                  Category
+                                </label>
+                                <div className="mt-2">
+                                  <select
+                                    id="category"
+                                    {...register('category')}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
+                                    >
+                                    <option value="Food">Food</option>
+                                    <option value="Utilities">Utilities</option>
+                                    <option value="Rent">Rent</option>
+                                    <option value="Transportation">Transportation</option>
+                                    <option value="Insurance">Insurance</option>
+                                    <option value="Wellness">Wellness</option>
+                                    <option value="Entertainment">Entertainment</option>
+                                    <option value="Other">Other</option>
+                                  </select>
+                                </div>
                               </div>
-                            </div>
-                          )}
+                            )}
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
                     type="button"
@@ -241,7 +226,7 @@ const CreateTransactionModal: React.FC<CreateTransactionModalProps> = ({
         </div>
       </Dialog>
     </Transition.Root>
-  );
+  )
 };
 
 export default CreateTransactionModal;
