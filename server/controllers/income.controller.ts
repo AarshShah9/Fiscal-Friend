6e7d968ee7b43ee3d9f2660c273950d56edce93f
AarshShah9 <aarshshah99@gmail.com>
2024-03-26 22:37:40 -0600

    Fixed income controller
    Signed-off-by: AarshShah9 <aarshshah99@gmail.com>

--- server/controllers/income.controller.ts
+++ server/controllers/income.controller.ts
@@ -2,65 +2,82 @@ import { User, Income } from '../models';
 import { Request, Response } from 'express';
 
 export const createIncome = async (req: Request, res: Response) => {
-    if (!req.body.user) {
-        return res.status(400).json({ success: false, message: 'User not authenticated' });
-    }
+  if (!req.body.user) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User not authenticated' });
+  }
 
-    if (!req.body.name || !req.body.amount || !req.body.recurring) {
-        return res.status(400).json({ success: false, message: 'Invalid request body' });
-    }
+  if (!req.body.name || !req.body.amount || !req.body.recurring) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Invalid request body' });
+  }
 
-    if (req.body.recurring === 'One-time' && !req.body.date) {
-        return res.status(400).json({ success: false, message: 'Invalid request body' });
-    }
+  if (req.body.recurring === 'One-time' && !req.body.date) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Invalid request body' });
+  }
 
-    if(req.body.recurring !== 'One-time') {
-        req.body.date = undefined;
-    }
+  if (req.body.recurring !== 'One-time') {
+    req.body.date = undefined;
+  }
 
-    try {
-        const newIncome = new Income({
-            user: req.body.user,
-            name: req.body.name,
-            amount: req.body.amount,
-            date: req.body.date,
-            recurring: req.body.recurring,
-        });
+  try {
+    const newIncome = new Income({
+      user: req.body.user,
+      name: req.body.name,
+      amount: req.body.amount,
+      date: req.body.date,
+      recurring: req.body.recurring,
+    });
 
-        await newIncome.save();
-        return res.status(201).json({ success: true, message: 'Income created', income: newIncome });
-    } catch (error) {
-        return res.status(500).json({ success: false, message: 'Server error', error });
-    }
+    await newIncome.save();
+    return res
+      .status(201)
+      .json({ success: true, message: 'Income created', income: newIncome });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Server error', error });
+  }
 };
 
 export const getIncomes = async (req: Request, res: Response) => {
-    if (!req.body.user) {
-        return res.status(400).json({ success: false, message: 'User not authenticated' });
-    }
+  if (!req.body.user) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User not authenticated' });
+  }
 
-    const incomes = await Income.find({ user: req.body.user });
+  const incomes = await Income.find({ user: req.body.user });
 
-    return res.status(201).json({ success: true, incomes });
+  return res.status(201).json({ success: true, incomes });
 };
 
 export const removeIncome = async (req: Request, res: Response) => {
-    if (!req.body.user) {
-        return res.status(400).json({ success: false, message: 'User not authenticated' });
-    }
+  if (!req.body.user) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'User not authenticated' });
+  }
 
-    const income = await Income.findOne({ _id: req.body.id, user: req.body.user });
+  const income = await Income.findOne({
+    _id: req.body.income,
+    user: req.body.user,
+  });
 
-    if (!income) {
-        return res.status(400).json({ success: false, message: 'Expense not found' });
-    }
+  if (!income) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Income not found' });
+  }
 
-    try {
-        await Income.findByIdAndDelete(income._id);
-        return res.status(201).json({ success: true, message: 'Income removed' });
-    } catch (error) {
-        return res.status(500).json({ success: false, message: 'Server error' });
-    }
+  try {
+    await Income.findByIdAndDelete(income._id);
+    return res.status(201).json({ success: true, message: 'Income removed' });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: 'Server error' });
+  }
 };
-
-
